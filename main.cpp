58e73c7eb6ef5e0a5e2e7595875cead73bc0a439
Julian Wiche <julian@wiche.eu>
2022-10-31 17:43:02 +0000

    'cleanup'

--- main.cpp
+++ main.cpp
@@ -30,52 +30,38 @@ int main(int argc, char** argv) {
 
     std::vector<string> args(argv + 1, argv + argc);
     string commands;
-    int command;
     int pin;
+    int delay;
     string s, tmp; 
-    vector<string> commandsVector;
-    command = 0;
-    pin = 6;
-
-    // Loop over command-line args
-    // (Actually I usually use an ordinary integer loop variable and compare
-    // args[i] instead of *i -- don't tell anyone! ;)
+    vector<string> commandsVector; 
+    pin = 6; // Used pin (WiringPiSchema)
+    delay= 100; // in milliseconds 
     for (auto i = args.begin(); i != args.end(); ++i) {
         if (*i == "-h" || *i == "--help") {
             cout << "Syntax: " << argv[0] << " -p <pin [6]> -c <command(s) [0x20(,0x21)]>" << endl;
             return 0;
         } else if (*i == "-c") {
-		
 	    commands = *++i;
 	    commandsVector = split(commands, ",");
-            command =  std::stoi(commands, nullptr, 0);
         } else if (*i == "-p") {
             pin =  std::stoi(*++i, nullptr, 0);
         }
     }
 
-   if(command == 0){
-	cout << "No command specified, use -h for help" << endl;
-	return 0;   
-   }
 
    
+ 
+ cout << "GPIO Pin is " << pin << endl;
 
-  wiringPiSetup();
-
-  cout << " Pin is " << pin << " command is:" << command << endl;
-
-
+ wiringPiSetup();
  OnkyoRI ori(pin);
 
-for(auto const& commandString: commandsVector) {
-int commandInteger = std::stoi(commandString, nullptr, 0);
-cout << "Sende befehl: " << commandInteger << endl;
-ori.send(commandInteger);	
-std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
-}
+   for(auto const& commandString: commandsVector) {
+	int commandInteger = std::stoi(commandString, nullptr, 0);
+	cout << "Sending command: " << commandInteger << endl;
+	ori.send(commandInteger);	
+	std::this_thread::sleep_for(std::chrono::milliseconds(delay)); 
+   }
 
-// ori.send(command);
-// ori.send(0x1AE);
 }
 
